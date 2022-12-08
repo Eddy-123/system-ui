f9c9d5f26340b5b14784f4edb604750db9fac1ff
Eddy-123 <adegnandjoueddy12@gmail.com>
2022-12-08 19:03:07 +0100

    create login and register forms

---
+++ src/components/frontend/auth/Register.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Navbar from '../../../layouts/frontend/Navbar'
+
+function Register() {
+  return (
+    <div>
+        <Navbar />
+        <div className='container py-5'>
+            <div className='row justify-content-center'>
+                <div className='col-md-6'>
+                    <div className='card'>
+                        <div className='card-header'>
+                            <h4>Register</h4>
+                        </div>
+                        <div className='card-body'>
+                            <form>
+                                <div className='form-group mb-3'>
+                                    <label>Name</label>
+                                    <input name='name' className='form-control' />
+                                </div>
+                                <div className='form-group mb-3'>
+                                    <label>Email</label>
+                                    <input name='email' className='form-control' />
+                                </div>
+                                <div className='form-group mb-3'>
+                                    <label>Password</label>
+                                    <input name='password' className='form-control' />
+                                </div>
+                                <div className='form-group mb-3'>
+                                    <label>Confirm password</label>
+                                    <input name='confirm_password' className='form-control' />
+                                </div>
+                                <div className='form-group mb-3'>
+                                    <button type='submit' className='btn btn-primary'>Register</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Register
